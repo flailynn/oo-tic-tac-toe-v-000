ab9426c12aeab49a2e2127751643216562bc4e86
Anita Beauchamp <amadamspr@gmail.com>
2018-07-14 22:07:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -95,11 +95,9 @@ end
 
 
 
-
-
 # #won? method determines if the game has been won by a player
 # returns array of win_combination if true or nil if false
-def won?(board)
+def won?
 WIN_COMBINATIONS.detect do |win_combination|
     #Load each win index into a variable
     win_index_1 = win_combination[0]
@@ -107,9 +105,9 @@ WIN_COMBINATIONS.detect do |win_combination|
     win_index_3 = win_combination[2]
 
     #Load the token (if any) at index position into a variable
-    position_1 = board[win_index_1].upcase
-    position_2 = board[win_index_2].upcase
-    position_3 = board[win_index_3].upcase
+    position_1 = @board[win_index_1].upcase
+    position_2 = @board[win_index_2].upcase
+    position_3 = @board[win_index_3].upcase
 
     # Determine whether positions contain winning X (or O) combination
     (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
@@ -117,8 +115,8 @@ WIN_COMBINATIONS.detect do |win_combination|
 end
 
 # #full? method determines if there are any open positions left on the board
-def full?(board)
-  board.none? do |position|
+def full?
+  @board.none? do |position|
     position == " " || position == "" || position.nil?
   end
 end
