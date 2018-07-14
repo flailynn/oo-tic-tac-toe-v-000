b06d7e98bd6a78cfcad37e01810b31003b59db11
Anita Beauchamp <amadamspr@gmail.com>
2018-07-14 22:23:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,20 +3,22 @@ class TicTacToe
     @board = board
   end
 
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
 end
 =begin
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
-=begin
+
+
 # display_board displays the Tic Tac Toe board
 def display_board
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
